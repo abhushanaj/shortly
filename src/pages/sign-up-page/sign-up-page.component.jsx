bf1d8f0dfd3450ad0aa031f455c548fdb945eb54
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-07-27 22:42:16 +0530

    Completion of the sign-up page for the application,by resuing sign-in page styling and forminput component

--- src/pages/sign-up-page/sign-up-page.component.jsx
+++ src/pages/sign-up-page/sign-up-page.component.jsx
@@ -1,9 +1,83 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import FormInput from "../../components/form-input/form-input.component";
+import CustomButton from "../../components/custom-button/custom-button.component";
+
+import signup from "../../asset/signup.png";
 
 import "./sign-up-page.styles.scss";
 
 const SignUpPage = () => {
-  return <h1>SignUpPage</h1>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+
+  return (
+    <>
+      <h1 className="signup-heading">Sign Up to to use our services!</h1>
+      <div className="u-container signin">
+        <form class="signin-form">
+          <div className="img__container">
+            <img src={signup} alt="Macbook Computer." />
+          </div>
+
+          <h2 className="sub-title">Signin using Email ID and Password</h2>
+
+          <div className="input__group">
+            <FormInput
+              type="text"
+              name="displayName"
+              value={displayName}
+              id="displayName"
+              placeholder="Enter your name..."
+              handleChange={(value) => setDisplayName(value)}
+            />
+          </div>
+
+          <div className="input__group">
+            <FormInput
+              type="email"
+              name="email"
+              value={email}
+              id="email"
+              placeholder="Enter your email..."
+              handleChange={(value) => setEmail(value)}
+            />
+          </div>
+
+          <div className="input__group">
+            <FormInput
+              type="password"
+              name="password"
+              value={password}
+              id="password"
+              placeholder="Enter your password..."
+              handleChange={(value) => setPassword(value)}
+            />
+          </div>
+
+          <div className="input__group">
+            <FormInput
+              type="password"
+              name="confirmPassword"
+              value={confirmPassword}
+              id="confirmPassword"
+              placeholder="Confirm your password..."
+              handleChange={(value) => setConfirmPassword(value)}
+            />
+          </div>
+
+          <CustomButton type="submit">Sign me up!</CustomButton>
+
+          <small>
+            <Link to="/signin">Already have an account?</Link>
+          </small>
+        </form>
+      </div>
+    </>
+  );
 };
 
 export default SignUpPage;
